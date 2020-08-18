3a544f54bf6a48be18a36ee9c879da965a201d68
Lucas Vanni <lucas.vanni@hotmail.com>
2020-08-17 23:39:13 -0300

    Desafio 01 completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,76 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).send({ error: 'Project is not found'})
+  }
+
+  const repo = {
+    id,
+    title,
+    url, 
+    techs,
+    likes: 0
+  }
+  
+  repositories[repoIndex] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if( repoIndex < 0 ) {
+    return response.status(400).json({ error: 'Repository id does not exists' });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repo = repositories.find(repository => repository.id === id);
+
+  if(!repo) {
+    return response.status(400).json({ error: 'Repository does not exists'});
+  }
+
+  repo.likes += 1;
+
+  return response.json(repo);
+
 });
 
 module.exports = app;
